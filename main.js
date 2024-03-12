15013a061b6cf6b5df04a7d998d0f7aea6d08a9f
mark <markpackham1@gmail.com>
2024-03-12 09:59:56 +0000

    Setup canvas size
    https://youtu.be/rTVoyWu8r6g?si=a3yBD4hDzi3cg5OO&t=536

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const canvas = document.querySelector("canvas");
+canvas.width = 1024;
+canvas.height = 576;
+
 // Canvas context for 2d API as opposed to a 3d one
 const c = canvas.getContext("2d");
 
 c.fillStyle = "white";
-c.fillRect(10, 10, 50, 50);
+c.fillRect(0, 0, canvas.width, canvas.height);
