446e231d491b4775b736e61e3640d611b836138a
mark <markpackham1@gmail.com>
2024-03-12 08:34:42 +0000

    Add white rectangle
    https://youtu.be/rTVoyWu8r6g?si=quMKVeVal0NK0TO1&t=454

---
+++ main.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector("canvas");
+// Canvas context for 2d API as opposed to a 3d one
+const c = canvas.getContext("2d");
+
+c.fillStyle = "white";
+c.fillRect(10, 10, 50, 50);
