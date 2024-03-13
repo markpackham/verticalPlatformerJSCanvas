782bdc687e35a4668d418a02f0f03f49a614190c
mark <markpackham1@gmail.com>
2024-03-13 11:36:58 +0000

    Add Player class
    https://youtu.be/rTVoyWu8r6g?si=VoGWbnEDtUkAHo8W&t=1461

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 const canvas = document.querySelector("canvas");
 canvas.width = 1024;
 canvas.height = 576;
-
 // Canvas context for 2d API as opposed to a 3d one
 const c = canvas.getContext("2d");
 
-c.fillStyle = "white";
-c.fillRect(0, 0, canvas.width, canvas.height);
-
 // Player
-c.fillStyle = "red";
-c.fillRect(200, 100, 100, 100);
+class Player {
+  constructor() {
+    this.position = {
+      x: 0,
+      y: 0,
+    };
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(200, y, 100, 100);
+  }
+}
+
+let y = 100;
+function animate() {
+  // Recursion
+  window.requestAnimationFrame(animate);
+  // Recreate canvas every single time animate() is called
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  y++;
+}
+
+animate();
