378d9bf733c6057441da5bb7d933b209d3ed4110
mark <markpackham1@gmail.com>
2024-03-14 16:26:46 +0000

    Add Sprite class
    https://youtu.be/rTVoyWu8r6g?si=kTpRC0_XffsX8JKP&t=2923

--- main.js
+++ main.js
@@ -7,6 +7,26 @@ canvas.height = 576;
 
 gravity = 0.5;
 
+// Sprite for drawing background & character art
+class Sprite {
+  constructor({ position, imageSrc }) {
+    this.position = position;
+    this.image = new Image();
+    this.image.src = imageSrc;
+  }
+
+  draw() {
+    // Don't waste time with an image that does not exist
+    if (!this.image) return;
+
+    c.drawImage(this.image, this.position.x, this.position.y);
+  }
+
+  update() {
+    this.draw();
+  }
+}
+
 // Player
 class Player {
   // We pass a new position (x & y Object) ever time we create a new player
