31b90bdaae7597c834a2d432b4c8838d3a6fbba8
mark <markpackham1@gmail.com>
2024-03-14 15:23:20 +0000

    Add gravity
    https://youtu.be/rTVoyWu8r6g?si=MQTIPOVmeDt7zVuP&t=1807

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
+// Canvas
 const canvas = document.querySelector("canvas");
-canvas.width = 1024;
-canvas.height = 576;
 // Canvas context for 2d API as opposed to a 3d one
 const c = canvas.getContext("2d");
+canvas.width = 1024;
+canvas.height = 576;
+
+gravity = 0.5;
 
 // Player
 class Player {
   // We pass a new position (x & y Object) ever time we create a new player
   constructor(position) {
     this.position = position;
+    // Since the character falls downwards by default we can ignore the "x" axis
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
   }
 
   draw() {
@@ -19,13 +27,15 @@ class Player {
 
   update() {
     this.draw();
-    this.position.y++;
+    this.position.y += this.velocity.y;
+    // Player falls faster over time
+    this.velocity.y += gravity;
   }
 }
 
 // Create Player
 const player = new Player({ x: 0, y: 0 });
-const player2 = new Player({ x: 0, y: 0 });
+const player2 = new Player({ x: 300, y: 100 });
 
 function animate() {
   // Recursion
