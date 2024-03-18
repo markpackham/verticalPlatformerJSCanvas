865ec024db9a0f5c53117f2a2ed594c532cbc223
mark <markpackham1@gmail.com>
2024-03-18 15:27:48 +0000

    Add check for horizontal collisions
    https://youtu.be/rTVoyWu8r6g?si=lKs60-dTqm2RG17A&t=6619

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -28,6 +28,8 @@ class Player {
     // Move left & right
     this.position.x += this.velocity.x;
 
+    this.checkForHorizontalCollisions();
+
     this.applyGravity();
 
     this.checkForVerticalCollisions();
@@ -40,6 +42,31 @@ class Player {
     // } else {
     //   this.velocity.y = 0;
     // }
+  }
+
+  checkForHorizontalCollisions() {
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      const collisionBlock = this.collisionBlocks[i];
+
+      if (
+        collision({
+          object1: this,
+          object2: collisionBlock,
+        })
+      ) {
+        if (this.velocity.x > 0) {
+          this.velocity.x = 0;
+
+          this.position.x = collisionBlock.position.x - this.width - 0.01;
+        }
+
+        if (this.velocity.x < 0) {
+          this.velocity.x = 0;
+          this.position.x =
+            collisionBlock.position.x + this.collisionBlock.width + 0.01;
+        }
+      }
+    }
   }
 
   applyGravity() {
@@ -61,9 +88,19 @@ class Player {
           object2: collisionBlock,
         })
       ) {
-        // Stop falling when hitting a block
+        // Stop falling when hitting a floor block
         if (this.velocity.y > 0) {
           this.velocity.y = 0;
+          // Make sure player falls ontop of block rather than through
+          // The 0.01 is to deal with horizontal collisions to factor in bottom of player & top of collision block
+          this.position.y = collisionBlock.position.y - this.height - 0.01;
+        }
+
+        // Don't allow player to jump up through a floor block
+        if (this.velocity.y < 0) {
+          this.velocity.y = 0;
+          this.position.y =
+            collisionBlock.position.y + this.collisionBlock.height + 0.01;
         }
       }
     }
--- js/main.js
+++ js/main.js
@@ -139,7 +139,7 @@ window.addEventListener("keydown", (event) => {
     // Jump
     case "w":
       keys.w.pressed = true;
-      player.velocity.y = -20;
+      player.velocity.y = -10;
       break;
 
     default:
