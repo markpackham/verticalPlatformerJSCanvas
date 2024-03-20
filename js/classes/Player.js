5811799afa8fda9237f3dc0fa9ba072658ff57f4
mark <markpackham1@gmail.com>
2024-03-20 10:51:08 +0000

    Finish off horizontal hitbox check
    https://youtu.be/rTVoyWu8r6g?si=c01VZbaYL2SbCIMw&t=9150

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -86,19 +86,25 @@ class Player extends Sprite {
           object2: collisionBlock,
         })
       ) {
+        // Right side
         if (this.velocity.x > 0) {
           this.velocity.x = 0;
-          this.position.x = collisionBlock.position.x - this.width - 0.01;
+
+          const offset =
+            this.hitbox.position.x - this.position.x + this.hitbox.width;
+
+          this.position.x = collisionBlock.position.x - offset - 0.01;
           break;
         }
 
+        // Left side
         if (this.velocity.x < 0) {
           this.velocity.x = 0;
 
-          const offset = this.hitbox.position.y - this.position.y;
+          const offset = this.hitbox.position.x - this.position.x;
 
           this.position.x =
-            collisionBlock.position.x + collisionBlock.width + 0.01;
+            collisionBlock.position.x + collisionBlock.width - offset + 0.01;
           break;
         }
       }
